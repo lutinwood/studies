bc0b5364779287d96a2b00268e59d4d852b37664
Thierry Forest <redmine@red-qual.(none)>
2014-11-20 17:42:29 +0100

    Absorbtion du plugin clruniv

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,27 @@ ActionController::Routing::Routes.draw do |map|
 map.resources :studies , :conditions => {:method => :post}
 #  map.study '', :controller => 'studies'
 
+ map.help 'help/index', :controller => 'help', :action => 'index', :conditions => {:method => :get}
+ 
+  map.signout 'logout', :controller => "sso", :action => "logout"
+
+#match 'sso/:cas_id'  => "sso#login":controller => "sso", :action => "login"
+
+# cette ligne pose un robleme avec d'autres liens
+#map.connect ':controller/:action/:back_url', :controller => "sso", :action => "login"
+
+  map.connect 'sso/:cas_id', :controller => "sso", :action => "login", :cas_id => /.+/
+  
+  map.with_options :controller => 'ldapsearch' do |ldapsearch|
+    ldapsearch.connect 'projects/:project_id/ldapsearch/add/:cas_id/:login', :action => 'addmember'
+    ldapsearch.connect 'projects/:project_id/ldapsearch', :action => 'index'
+  end
+
+  map.with_options :controller => 'sys' do |sys|
+    sys.connect 'sys/reposman_requests.:format', :action => 'requests', :conditions => {:method => :get}
+    sys.connect 'sys/reposman_requests/:id/close.:format', :action => 'close_request', :conditions => {:method => :post}
+  end
+
+#map.connect 'auth_sources/:id/test_connection', :controller => 'auth_source', :action => 'test_connection'
 end
+
--- init.rb
+++ init.rb
@@ -1,14 +1,35 @@
 require 'redmine'
+require 'dispatcher'
+
+require_dependency 'cas'
+
+require 'user_patch'
+require 'account_controller_patch'
+
+Dispatcher.to_prepare :redmine_clruniv do
+  Principal.send(:include, PrincipalPatch)
+  User.send(:include, UserPatch)
+end
 
 Redmine::Plugin.register :redmine_studies do
-  name 'Redmine Studies plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Redmine Insertiion de filière (studies)'
+  author 'TF'
+  description 'Insertion des etudiants'
+  version '0.0.2'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 
+  
+  permission :add_studies, :studies => :index
+  
+  
+  
+  delete_menu_item :account_menu, :register
+  delete_menu_item :top_menu, :help 
+  
+  menu :top_menu, :faq, {:controller=>'help', :action=>'index'}, :last => true 
   menu :admin_menu, :studies, { :controller => 'studies', :action => 'index' }
 
-  permission :add_studies, :studies => :index
+  
+  requires_redmine :version_or_higher => '1.3.0'
 end
