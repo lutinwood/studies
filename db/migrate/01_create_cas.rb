508a448f5b67334be5b54d97b34ae402861146cd
Thierry Forest <redmine@red-qual.(none)>
2014-11-21 18:04:40 +0100

    version fonctionnelle

--- db/migrate/01_create_cas.rb
+++ db/migrate/01_create_cas.rb
@@ -7,16 +7,10 @@ class CreateCas < ActiveRecord::Migration
       t.column "ldap", :string, :limit => 60
       t.column "dn", :string, :limit => 255
     end
-    add_column :users, :cas_id, :integer
-    add_column :users, :supann_affectation_first, :string, :limit => 20
-    add_column :users, :supann_affectation_last, :string, :limit => 20
-    add_column :users, :aua_statut, :string , :limit => 10
-    add_column :users, :aua_millesime, :string, :limit => 60
   end
 
   def self.down
     drop_table :cas
-    remove_columns :users, :cas_id
   end
 
 end
