8913e56184e2fea79453d80f0e511b1f444c71c2
Thierry Forest <Thierry.forest@univ-angers.fr>
2015-01-06 14:44:31 +0100

    Installation du plugin sur un redmine 2.4.7 OK : Script rake redmine:plugins:migrate RAILS_ENV=development fonctionel

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,20 @@
-ActionController::Routing::Routes.draw do |map|
-map.resources :studies , :conditions => {:method => :post}
-#  map.study '', :controller => 'studies'
-
- map.help 'help/index', :controller => 'help', :action => 'index', :conditions => {:method => :get}
- 
-  map.signout 'logout', :controller => "sso", :action => "logout"
-
-#match 'sso/:cas_id'  => "sso#login":controller => "sso", :action => "login"
-
-# cette ligne pose un robleme avec d'autres liens
-#map.connect ':controller/:action/:back_url', :controller => "sso", :action => "login"
-
-  map.connect 'sso/:cas_id', :controller => "sso", :action => "login", :cas_id => /.+/
+#ActionController::Routing::Routes.draw do |map|
+RedmineApp::Application.routes.draw do
   
-  map.with_options :controller => 'ldapsearch' do |ldapsearch|
-    ldapsearch.connect 'projects/:project_id/ldapsearch/add/:cas_id/:login', :action => 'addmember'
-    ldapsearch.connect 'projects/:project_id/ldapsearch', :action => 'index'
+  #match "/studies" => "studies#index", :via =>[:get]
+  controller 'studies' do
+    get 'studies' => :index
   end
 
-  map.with_options :controller => 'sys' do |sys|
-    sys.connect 'sys/reposman_requests.:format', :action => 'requests', :conditions => {:method => :get}
-    sys.connect 'sys/reposman_requests/:id/close.:format', :action => 'close_request', :conditions => {:method => :post}
+  # create, show, show_user_list, user_list, index, destroy, define_user 
+  # destroy_users 
+  
+  get '/help' => 'help#index'
+  get '/logout' => 'sso#logout'
+  get 'sso/:cas_id' => 'sso#cas_id'
+  
+  controller 'ldapsearch' do
+    get 'projects/:project_id/ldapsearch/add/:cas_id/:login' => :addmember
+    get 'projects/:project_id/ldapsearch' => :index
   end
-
-#map.connect 'auth_sources/:id/test_connection', :controller => 'auth_source', :action => 'test_connection'
 end
-
